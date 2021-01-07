cda437fc0db900345ebb42dacca58a60a0ea628e
MateuszZam <mzamiara1@edu.cdv.pl>
2021-01-07 22:27:01 +0100

    lab 2 zakonczony

---
+++ lab2/mzamiaralab2.WebApi/Context/AzureDbContext.cs
@@ -0,0 +1,24 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using mzamiaralab2.WebApi.Models;
+
+namespace mzamiaralab2.WebApi.Context
+{
+    public class AzureDbContext : DbContext
+    {
+        public AzureDbContext(DbContextOptions<AzureDbContext> options) : base(options)
+        {
+        }
+
+        protected AzureDbContext()
+        {
+        }
+
+        public DbSet<Person> People { get; set; }
+    }
+}
+
